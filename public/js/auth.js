f18b7aa909150f3855d5741314b80664593d2100
JeraldineCagatin <cagatinjeraldineb@email.com>
2021-01-13 11:37:17 +0800

    login/logout

---
+++ public/js/auth.js
@@ -0,0 +1,108 @@
+// REGISTER NEW USER
+const signupForm = document.querySelector('#signup-form');
+
+signupForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const email = signupForm['signup-email'].value;
+    const password = signupForm['signup-password'].value;
+
+    auth.createUserWithEmailAndPassword(email, password).then((user) => {
+        if (user) {
+            console.log(user);
+        }
+    });
+});
+
+// LOGIN USER
+const loginForm = document.querySelector('#login-form');
+
+loginForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const email = loginForm['login-email'].value;
+    const password = loginForm['login-password'].value;
+
+    auth.signInWithEmailAndPassword(email, password).then((user) => {
+        if (user) {
+            console.log(user);
+        }
+    });
+});
+
+// GOOGLE LOGIN
+const googleSignIn = document.querySelector('#googleSignIn')
+googleSignIn.addEventListener('click', (e) => {
+    var provider = new firebase.auth.GoogleAuthProvider();
+
+    auth.signInWithPopup(provider)
+        .then((result) => {
+            /** @type {firebase.auth.OAuthCredential} */
+            var credential = result.credential;
+            var token = credential.accessToken;
+            var user = result.user;
+        }).catch((error) => {
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            var email = error.email;
+            var credential = error.credential;
+        });
+});
+
+// FACEBOOK LOGIN
+const facebookSignIn = document.querySelector('#facebookSignIn')
+facebookSignIn.addEventListener('click', (e) => {
+    var provider = new firebase.auth.FacebookAuthProvider();
+
+    auth.signInWithPopup(provider)
+        .then((result) => {
+            /** @type {firebase.auth.OAuthCredential} */
+            var credential = result.credential;
+            var user = result.user;
+            var accessToken = credential.accessToken;
+        })
+        .catch((error) => {
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            var email = error.email;
+            var credential = error.credential;
+        });
+});
+
+// setup guides
+const loggedOutlinks = document.querySelectorAll('.logged-out');
+const loggedInlinks = document.querySelectorAll('.logged-in');
+
+const setupUI = (user) => {
+    if (user) {
+        //toggle UI elements
+        loggedInlinks.forEach(item => item.style.display = 'block');
+        loggedOutlinks.forEach(item => item.style.display = 'none');
+    }
+
+    else {
+        //toggle UI elements
+
+        loggedInlinks.forEach(item => item.style.display = 'none');
+        loggedOutlinks.forEach(item => item.style.display = 'block');
+    }
+};
+
+// Get the currently signed-in user
+auth.onAuthStateChanged((user) => {
+    if (user) {
+        console.log(user);
+        setupUI(user);
+    } else {
+        console.log('no user logged in');
+        setupUI();
+    }
+});
+
+// signout user
+const signOut = document.querySelector('#signout');
+signout.addEventListener('click', (e) => {
+    e.preventDefault();
+    auth.signOut();
+});
+
---
+++ public/js/index.js
@@ -0,0 +1,34 @@
+  
+
+
+// REVEAL NAVBAR HAMBURGER
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems, options);
+  });
+
+  $(document).ready(function(){
+    $('.sidenav').sidenav();
+  });
+
+
+
+//   <nav>
+//     <div class="nav-wrapper">
+//       <a href="#!" class="brand-logo">Logo</a>
+//       <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+//       <ul class="right hide-on-med-and-down">
+//         <li><a href="sass.html">Sass</a></li>
+//         <li><a href="badges.html">Components</a></li>
+//         <li><a href="collapsible.html">Javascript</a></li>
+//         <li><a href="mobile.html">Mobile</a></li>
+//       </ul>
+//     </div>
+//   </nav>
+
+//   <ul class="sidenav" id="mobile-demo">
+//     <li><a href="sass.html">Logo</a></li>
+//     <li><a href="badges.html">Components</a></li>
+//     <li><a href="collapsible.html">Javascript</a></li>
+//     <li><a href="mobile.html">Mobile</a></li>
+//   </ul>
