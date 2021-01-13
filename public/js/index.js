6f34392be820e605327e78d5189fa182b70519c9
JeraldineCagatin <cagatinjeraldineb@email.com>
2021-01-14 04:22:01 +0800

    change index

--- public/js/index.js
+++ public/js/index.js
@@ -1,29 +1,32 @@
   // setup guides
-const loggedOutlinks = document.querySelectorAll('.logged-out');
-const loggedInlinks = document.querySelectorAll('.logged-in');
+  const loggedOutlinks = document.querySelectorAll('.logged-out');
+  const loggedInlinks = document.querySelectorAll('.logged-in');
 
-const setupUI = (user) => {
-    if (user) {
-        //toggle UI elements
-        loggedInlinks.forEach(item => item.style.display = 'block');
-        loggedOutlinks.forEach(item => item.style.display = 'none');
-    }
+  const setupUI = (user) => {
+      if (user) {
+          //toggle UI elements
+          loggedInlinks.forEach(item => item.style.display = 'block');
+          loggedOutlinks.forEach(item => item.style.display = 'none');
+      } else {
+          //toggle UI elements
+          loggedInlinks.forEach(item => item.style.display = 'none');
+          loggedOutlinks.forEach(item => item.style.display = 'block');
+      }
+  };
 
-    else {
-        //toggle UI elements
+  // Get the currently signed-in user
+  auth.onAuthStateChanged((user) => {
+      if (user) {
+          console.log(user);
+          setupUI(user);
+      } else {
+          console.log('no user logged in');
+          setupUI();
+      }
+  });
 
-        loggedInlinks.forEach(item => item.style.display = 'none');
-        loggedOutlinks.forEach(item => item.style.display = 'block');
-    }
-};
-
-// Get the currently signed-in user
-auth.onAuthStateChanged((user) => {
-  if (user) {
-      console.log(user);
-      setupUI(user);
-  } else {
-      console.log('no user logged in');
-      setupUI();
-  }
-});
+  //NAVBAR
+  document.addEventListener('DOMContentLoaded', function() {
+      var elems = document.querySelectorAll('.sidenav');
+      var instances = M.Sidenav.init(elems, options);
+  });
--- public/js/register.js
+++ public/js/register.js
@@ -10,10 +10,38 @@ signupForm.addEventListener('submit', (e) => {
     auth.createUserWithEmailAndPassword(email, password).then((user) => {
         if (user) {
             window.location = 'index.html';
-            document.getElementById('userName').innerHTML=user.email;
+            alert("Sign-Up Succesful!")
         }
         else {
             console.log('NO USER');
         }
     });
 });
+
+
+
+function checkUserFullName(){
+    var userSurname = document.getElementById("userFullName").value;
+    var flag = false;
+    if(userSurname === ""){
+        flag = true;
+    }
+    if(flag){
+        document.getElementById("userFullNameError").style.display = "block";
+    }else{
+        document.getElementById("userFullNameError").style.display = "none";
+    }
+}
+
+function checkUserSurname(){
+    var userSurname = document.getElementById("userSurname").value;
+    var flag = false;
+    if(userSurname === ""){
+        flag = true;
+    }
+    if(flag){
+        document.getElementById("userSurnameError").style.display = "block";
+    }else{
+        document.getElementById("userSurnameError").style.display = "none";
+    }
+}
