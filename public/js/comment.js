c8b2ebb0945f63b0d735b03951b38ae8bde42173
Kianibal <knrbaliwagan@iskolarngbayan.pup.edu.ph>
2021-01-14 17:18:54 +0800

    show comment

--- public/js/comment.js
+++ public/js/comment.js
@@ -28,3 +28,52 @@ const postComment = (user) => {
     });
 
 }
+
+const commentSection = document.querySelector('.comment');
+
+function showComment(doc) {
+    let html = [
+        `
+
+        <div class="col l3 m4 s12" id="${doc.id}">
+            <div class="card">
+                <div class="card-reveal">
+                    <span class="card-title grey-text text-darken-4">${doc.data().commentContent}
+                    <i class="material-icons right">close</i>
+                    </span>
+                </div>
+                <div class="card-content"><p style="padding-left: 1vw">By: ${doc.data().commentAuthor}</p></div>
+                <span class="left">
+                    <a class="waves-effect waves-light btn modal-trigger" href="#modal1">Modal</a>
+                </span>
+                <span class="right">
+                    <a id="delBtn" class="waves-effect waves-light btn"><i class="material-icons left">delete</i>Delete</a>
+                </span>
+            </div>
+        </div>
+        `].join('');
+
+    const div = document.createElement('div');
+    div.innerHTML = html;
+    commentSection.appendChild(div);
+}
+
+const ProfileView = (user) => {
+    userId = user.uid;
+    db.collection('comment').get().then(snapshot => {
+        snapshot.docs.forEach((doc) => {
+                showComment(doc);
+            });
+    });
+}
+
+
+auth.onAuthStateChanged((user) => {
+    if (user) {
+        ProfileView(user);
+    }
+
+    else {
+
+    }
+});
